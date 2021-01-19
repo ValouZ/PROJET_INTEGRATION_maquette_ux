22d52362745fc743d3b8a0c4ecb210163c530e50
ValouZ <debray.va@gmail.com>
2021-01-20 00:20:43 +0100

    responsive

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const burger = document.getElementById("app-burger");
+const filter = document.getElementById("app-filter");
 
-burger.addEventListener("click", displayMenu);
+
+// Event listener used to display the content of the burger menu
+// if the window is under 992px of width, we add the event listener 
+// attached to the displayBurgerMenu function
+if (window.innerWidth < 992)
+    burger.addEventListener("click", displayMenu);
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 992)
+        burger.addEventListener("click", displayMenu);
+    else
+        burger.removeEventListener("click", displayMenu);
+        filter.classList.remove("display");
+        burger.classList.remove("display");
+});
 
 function displayMenu(){
-  burger.classList.toggle("display")
+  burger.classList.toggle("display");
+  filter.classList.toggle("display");
 }
