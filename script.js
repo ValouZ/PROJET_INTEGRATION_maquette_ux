f0f9f9a9d6de54c7920edc49131d32c169142de9
ValouZ <debray.va@gmail.com>
2021-01-20 23:10:24 +0100

    test caroussel on mobile

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const burger = document.getElementById("app-burger");
 const filter = document.getElementById("app-filter");
-// const nextButton = document.getElementsByClassName("next-button")[0];
-// const previousButton = document.getElementsByClassName("previous-button")[0];
+const nextButton = document.getElementsByClassName("next-button")[0];
+const previousButton = document.getElementsByClassName("previous-button")[0];
+const carouSlides = document.getElementsByClassName("slides")[0];
+const numberOfImages = carouSlides.children.length;
 const slides = document.getElementsByClassName("content")[0];
 const linksSlides = slides.children;
 let numberOfLinksSlides = linksSlides.length;
 let sliderCounter = 0;
+let carouCounter = 0;
 
 // Event listener used to display the content of the burger menu
 // if the window is under 992px of width, we add the event listener 
@@ -22,8 +25,8 @@ window.addEventListener("resize", () => {
   burger.classList.remove("display");
 });
 
-// nextButton.addEventListener("click", next);
-// previousButton.addEventListener("click", previous)
+nextButton.addEventListener("click", nextCarou);
+previousButton.addEventListener("click", prevCarou)
 
 setInterval(nextSlider, 5000);
 
@@ -35,14 +38,35 @@ function displayMenu() {
 
 function nextSlider() {
   sliderCounter++;
-  if (sliderCounter > numberOfLinksSlides -1) {
+  if (sliderCounter > numberOfLinksSlides - 1) {
     sliderCounter = 0;
   }
   updateSlider();
 }
 
 function updateSlider() {
-    console.log(sliderCounter)
   const transform = -100 * sliderCounter;
   slides.style.transform = `translateX(${transform}vw)`;
+}
+
+function nextCarou() {
+  carouCounter++;
+  if (carouCounter > numberOfImages - 1) {
+    carouCounter = 0;
+  }
+  updateCarou();
+}
+
+function prevCarou() {
+  carouCounter--;
+  if (carouCounter < 0) {
+    carouCounter = numberOfImages - 1;
+  }
+  updateCarou();
+}
+
+function updateCarou() {
+  const transform = -50 * carouCounter;
+  carouSlides.style.transform = `translateX(${transform}%)`;
+  console.log(carouCounter);
 }
